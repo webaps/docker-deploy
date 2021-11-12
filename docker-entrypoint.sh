dbea4dccea3d0cea799a613315bdf94fac0bd4f4
nickverschoor <nick@verschoor.cc>
2021-11-12 23:25:25 +0100

    Setup action

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 set -eu
 
 color_yellow='\033[33;1m'
@@ -27,6 +27,10 @@ fi
 if [ -z "$INPUT_DEPLOY_PATH" ]; then
     echo "Input deploy_path is required!"
     exit 1
+fi
+
+if [ -z "$INPUT_STACK_FILE_NAME" ]; then
+  INPUT_STACK_FILE_NAME=docker-compose.yaml
 fi
 
 SSH_COMMAND="ssh -q -t -i $HOME/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
@@ -58,24 +62,32 @@ execute_ssh "mkdir -p $INPUT_DEPLOY_PATH || true"
 echo -e "${color_yellow}> Transfer files to destination folder${color_reset}"
 rsync -rzhp --delete --rsync-path="sudo rsync" -e="$SSH_COMMAND" --info=progress2 ./ "$INPUT_DOCKER_HOST":"$INPUT_DEPLOY_PATH"
 
-if ! [ -z "$INPUT_PRE_DEPLOY_COMMAND" ] ; then
+if [ -n "$INPUT_PRE_DEPLOY_COMMAND" ] ; then
   echo -e "${color_yellow}> Running pre-deploy command${color_reset}"
   execute_ssh "cd $INPUT_DEPLOY_PATH && $INPUT_PRE_DEPLOY_COMMAND" 2>&1
 fi
 
-if ! [ -z "$INPUT_PULL_IMAGES" ] && [ $INPUT_PULL_IMAGES = 'true' ] ; then
+if [ -n "$INPUT_PULL_IMAGES" ] && [ $INPUT_PULL_IMAGES = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
   echo -e "${color_yellow}> Pulling images${color_reset}"
   execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f ./$INPUT_DOCKER_COMPOSE_FILE pull" 2>&1
 fi
 
-if ! [ -z "$INPUT_BUILD_IMAGES" ] && [ $INPUT_BUILD_IMAGES = 'true' ] ; then
+if [ -n "$INPUT_BUILD_IMAGES" ] && [ $INPUT_BUILD_IMAGES = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
   echo -e "${color_yellow}> Building images${color_reset}"
   execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f ./$INPUT_DOCKER_COMPOSE_FILE build --no-cache" 2>&1
 fi
 
 echo -e "${color_yellow}> Deploy${color_reset}"
-execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f ./$INPUT_DOCKER_COMPOSE_FILE $INPUT_ARGS --force-recreate --remove-orphans" 2>&1
+#execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f ./$INPUT_DOCKER_COMPOSE_FILE $INPUT_ARGS --force-recreate --remove-orphans" 2>&1
 
+case $INPUT_DEPLOYMENT_MODE in
+  docker-swarm)
+    execute_ssh "cd $INPUT_DEPLOY_PATH && docker $INPUT_ARGS stack deploy --compose-file $INPUT_STACK_FILE_NAME" 2>&1
+  ;;
 
+  *)
+    execute_ssh "cd $INPUT_DEPLOY_PATH && docker-compose -f ./$INPUT_STACK_FILE_NAME $INPUT_ARGS --force-recreate --remove-orphans" 2>&1
+  ;;
+esac
 
 
